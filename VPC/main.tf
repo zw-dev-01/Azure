db91d209f4998cc29a7edc756a17e15b4e058e72
Zbigi W <zw-dev@outlook.com>
2024-08-21 14:08:22 +0200

    update ignored files

---
+++ VPC/main.tf
@@ -0,0 +1,18 @@
+
+
+resource "azurerm_virtual_network" "zwdev_vnet" {
+  name                = "zwdev-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.azure_location
+  resource_group_name = var.azure_resource_group
+}
+
+resource "azurerm_subnet" "vnet_subnet_1" {
+  name                 = "subnet-1"
+  resource_group_name  = var.azure_resource_group
+  virtual_network_name = "zwdev-vnet"
+  address_prefixes     = ["10.0.1.0/24"]
+  depends_on = [
+    azurerm_virtual_network.zwdev_vnet
+  ]
+}
